545ae767f0dbac27dcfd2027dc6d78528adcd590
ched-brgb <chedly@flowlabs.ai>
2021-08-05 17:47:59 -0700

    add tile

--- vendor/assets/leaflet/leaflet.osm.js
+++ vendor/assets/leaflet/leaflet.osm.js
@@ -2,7 +2,7 @@ L.OSM = {};
 
 L.OSM.TileLayer = L.TileLayer.extend({
   options: {
-    url: 'https://18.144.60.238/{z}/{x}/{y}.png',
+    url: 'https://18.144.60.238/tile/{z}/{x}/{y}.png',
     attribution: '© <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors'
   },
 
@@ -14,7 +14,7 @@ L.OSM.TileLayer = L.TileLayer.extend({
 
 L.OSM.Mapnik = L.OSM.TileLayer.extend({
   options: {
-    url: 'https://18.144.60.238/{z}/{x}/{y}.png',
+    url: 'https://18.144.60.238/tile/{z}/{x}/{y}.png',
     maxZoom: 19
   }
 });
